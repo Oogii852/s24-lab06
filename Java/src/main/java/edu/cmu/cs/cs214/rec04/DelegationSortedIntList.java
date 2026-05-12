67036db46668c1970cff58c272daa32fc8210481
Oogii852 <otgonsukh0311@gmail.com>
2026-05-12 11:17:18 +0800

    Fix CI: set working directory to Java

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,48 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList {
+    private SortedIntList delegate;
+    private int totalAdded;
 
+    public DelegationSortedIntList() {
+        this.delegate = new SortedIntList();
+        this.totalAdded = 0;
+    }
+    
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return delegate.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        totalAdded += list.size();
+        return delegate.addAll(list);
+    }
+
+    @Override
+    public int get(int index) {
+        return delegate.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return delegate.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return delegate.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
